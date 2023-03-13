4952628d2b953b68be6faab807af884a3da0aa45
mahtabesmaeili1 <mahtabesmaeilii13@gmail.com>
2023-03-13 11:23:43 +0100

    first commit

---
+++ src/components/Navigation.js
@@ -0,0 +1,87 @@
+import "../Styles/Navigation.css";
+import { useState } from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+export const Navigation = () => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <Nav>
+      <Logo href="/">[ AMOK ]</Logo>
+      <Hamburger onClick={() => setOpen(!open)}>
+        <span />
+        <span />
+        <span />
+      </Hamburger>
+      <Menu open={open}>
+        <Link className="hover-underline-animation" to="#">
+          Home
+        </Link>
+        <Link className="hover-underline-animation" to="#">
+          Contact
+        </Link>
+      </Menu>
+    </Nav>
+  );
+};
+
+const Nav = styled.div`
+  padding: 0 2rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-wrap: wrap;
+  background: black;
+  /* position: absolute; */
+  top: 0;
+  left: 0;
+  right: 0;
+`;
+
+const Logo = styled.a`
+  padding: 1rem 0;
+  color: #ececec;
+  text-decoration: none;
+  font-weight: 800;
+  font-size: 1.7rem;
+
+  span {
+    font-weight: 300;
+    font-size: 1.3rem;
+  }
+  @media (max-width: 780px) {
+    font-size: 14px;
+  }
+`;
+
+const Hamburger = styled.div`
+  display: none;
+  flex-direction: column;
+  cursor: pointer;
+  span {
+    height: 2px;
+    width: 25px;
+    background-color: #ececec;
+    margin-bottom: 4px;
+    border-radius: 5px;
+  }
+
+  @media (max-width: 780px) {
+    display: flex;
+  }
+`;
+
+const Menu = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: relative;
+
+  @media (max-width: 780px) {
+    overflow: hidden;
+    flex-direction: column;
+    width: 100%;
+    max-height: ${({ open }) => (open ? "300px" : "0")};
+    transition: max-height 0.3s ease-in;
+  }
+`;
---
+++ src/pages/Homepage.js
@@ -0,0 +1,9 @@
+import "../App.css";
+
+export const Homepage = () => {
+  return (
+    <div>
+      <p className="title"> AMOK</p>
+    </div>
+  );
+};
